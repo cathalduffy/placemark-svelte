cc5d22c2eaede271e89da0a1f51b02abaa2d90ba
Cathal Duffy <cathalduffy@cathals-mbp.home>
2022-06-03 16:43:09 +0100

    Add chart for categories

--- src/components/CategoryChart.svelte
+++ src/components/CategoryChart.svelte
@@ -4,6 +4,8 @@
   import {getContext, onMount} from 'svelte'
   import Chart from 'svelte-frappe-charts';
   const placemarkService = getContext("PlacemarkService");
+  const categoryService = getContext("CategoryService");
+
 
   let url = ``;
 
@@ -17,19 +19,44 @@
     ]
   };
 
+  let totalByCategory = {
+    labels: [],
+    datasets: [
+      {
+        values: []
+      }
+    ]
+  }
+
   onMount(async (request) => {
     url = window.location.href
     console.log(url)
     let parsedURL = url.substring(33)
-    placemarkList = await placemarkService.getPlacemarks(parsedURL);;
+    placemarkList = await placemarkService.getPlacemarks(parsedURL);
     placemarkList.forEach(placemark => {
       if (placemark.name == "Tramore") {
         data.datasets[0].values[0] += placemark.amenitiesRating
       } else if (placemark.name == "Dunmore") {
         data.datasets[0].values[1] += placemark.amenitiesRating
       }
+    });
+
+    let categories = await categoryService.getCategories();
+    totalByCategory.labels = [];
+    categories.forEach(category => {
+      totalByCategory.labels.push(`${category.title}`)
+      totalByCategory.datasets[0].values.push(0);
     })
+    categories.forEach((category, i) => {
+      placemarkList.forEach(placemark => {
+        if (placemark.categoryid == category._id) {
+          totalByCategory.datasets[0].values[i] += placemark.amenitiesRating;
+        }
+        console.log(placemark.categoryid._id)
+      });
+    });
   });
+
 
 </script>
 
@@ -37,5 +64,9 @@
   <div class="column box has-text-centered">
     <h1 class="title is-4">Placemark Ratings</h1>
     <Chart data={data} type="bar"/>
+  </div>
+    <div class="column has-text-centered">
+    <h1 class="title is-4">By Category</h1>
+    <Chart data={totalByCategory} type="bar"/>
   </div>
 </div>
